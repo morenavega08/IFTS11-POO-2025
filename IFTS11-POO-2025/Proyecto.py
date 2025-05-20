b89ca3bcef54145488f8fe11b22d3666bd1dac7d
morenavega08 <08vegamorena@gmail.com>
2025-05-20 20:00:59 -0300

    Add files via upload

---
+++ IFTS11-POO-2025/Proyecto.py
@@ -0,0 +1,12 @@
+class Perro(object):
+    def __init__(self, nombre, raza, edad, tamaño, peso, estado_salud, vacunado, estado, temperamento, id):
+        self.nombre = nombre
+        self.raza = raza
+        self.edad = edad
+        self.tamaño = tamaño
+        self.peso = peso
+        self.estado_salud = estado_salud
+        self.vacunado = vacunado
+        self.estado = estado
+        self.temperamento = temperamento
+        self.id = id
